f5b155d1be952422edb180da637fc8a79ffb0077
lc <837948166@qq.com>
2023-11-21 18:34:04 +0800

    超过索引bug修复

--- src/main/java/com/xw/cloud/bean/Virtual.java
+++ src/main/java/com/xw/cloud/bean/Virtual.java
@@ -1,12 +1,24 @@
 package com.xw.cloud.bean;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Builder
 @Getter
 @ToString
 public class Virtual {
+    @JsonProperty(value = "id")
     private int id;
+    @JsonProperty(value = "name")
     private String name;
+    @JsonProperty(value = "state")
     private String state;
+    @JsonProperty(value = "maxMem")
+    private String maxMem;
+    @JsonProperty(value = "useMem")
+    private String useMem;
+    @JsonProperty(value = "cpuNum")
+    private String cpuNum;
+    @JsonProperty(value = "cpuTime")
+    private String cpuTime;
 }
