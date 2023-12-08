ace2e96dd13832403bcdb04ebdeeff9c2106369c
Chong Lv <67569851+lc837948166@users.noreply.github.com>
2023-12-08 11:03:57 +0800

    解决Builder冲突

--- src/main/java/com/xw/cloud/bean/VMInfo2.java
+++ src/main/java/com/xw/cloud/bean/VMInfo2.java
@@ -1,10 +1,15 @@
 package com.xw.cloud.bean;
 
-import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //@Getter
 //@ToString
@@ -14,70 +19,27 @@ import lombok.Data;
 /**
  * 虚拟机信息
  */
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
-@TableName(value = "T_Cloud_VM")
+@TableName(value = "T_CLOUD_VM")
 @ApiModel(description = "虚拟机信息")
 public class VMInfo2 {
-
-    @TableField(value = "name")
+    @TableId(type = IdType.AUTO)
+    @JsonProperty(value = "name")
     @ApiModelProperty(value = "虚拟机名称")
     private String name;
-
-    @TableField(value = "ip")
+    @JsonProperty(value = "ip")
     @ApiModelProperty(value = "虚拟机ip")
     private String ip;
-
-    @TableField(value = "username")
+    @JsonProperty(value = "username")
     @ApiModelProperty(value = "用户名")
     private String username;
-
-    @TableField(value = "passwd")
+    @JsonProperty(value = "passwd")
     @ApiModelProperty(value = "密码")
     private String passwd;
-
-    @TableField(value = "serverip")
+    @JsonProperty(value = "serverip")
     @ApiModelProperty(value = "虚拟机所在服务器ip")
     private String serverip;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPasswd() {
-        return passwd;
-    }
-
-    public void setPasswd(String passwd) {
-        this.passwd = passwd;
-    }
-
-    public String getServerip() {
-        return serverip;
-    }
-
-    public void setServerip(String serverip) {
-        this.serverip = serverip;
-    }
-
-
 }
