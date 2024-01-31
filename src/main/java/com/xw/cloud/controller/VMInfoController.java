7a0f4637e10619675456015b8dd91a1df1e6b19d
lc837948166 <837948166@qq.com>
2024-01-31 13:13:36 +0800

    修改端口

--- src/main/java/com/xw/cloud/controller/VMInfoController.java
+++ src/main/java/com/xw/cloud/controller/VMInfoController.java
@@ -137,10 +137,12 @@ public class VMInfoController {
         List<Map<String, Object>> modifiedList = new ArrayList<>();
         List<Integer> fixedNumbers = new ArrayList<>();
         fixedNumbers.add(8000);
-        fixedNumbers.add(8050);
+        fixedNumbers.add(8085);
         fixedNumbers.add(7051);
         fixedNumbers.add(7052);
         fixedNumbers.add(7053);
+
+        Integer[] fixed2={8000,8085,8002,8003,8004};
 
         for (VMInfo2 item : tempList) {
             if (item.getHostport() == null) {
@@ -158,6 +160,7 @@ public class VMInfoController {
                 modifiedList.add(modifiedEntry);
 
             } else {
+
                 int hostport = item.getHostport();
                 Map<String, Object> modifiedEntry = new HashMap<>();
                 modifiedEntry.put("name", item.getName());
@@ -169,17 +172,18 @@ public class VMInfoController {
                 modifiedEntry.put("memory", item.getMemory());
                 modifiedEntry.put("status", item.getStatus());
                 List<Map<String, Object>> hostportList = new ArrayList<>();
-                for (int i = 0; i < fixedNumbers.size(); i++) {
-                    Map<String, Object> hostportEntry = new HashMap<>();
-                    hostportEntry.put(String.valueOf(fixedNumbers.get(i)), String.valueOf(hostport + i));
-                    hostportList.add(hostportEntry);
-                    if(i==fixedNumbers.size()-1) {
-                        Map<String, Object> hostportEntry2 = new HashMap<>();
-                        hostportEntry2.put(String.valueOf(8085), String.valueOf(8085));
-                        hostportList.add(hostportEntry2);
-                        Map<String, Object> hostportEntry3 = new HashMap<>();
-                        hostportEntry3.put(String.valueOf(8086), String.valueOf(8086));
-                        hostportList.add(hostportEntry3);
+                if(item.getIp().equals("10.0.8.2")){
+                    for (int i = 0; i < fixedNumbers.size(); i++) {
+                        Map<String, Object> hostportEntry = new HashMap<>();
+                        hostportEntry.put(String.valueOf(fixedNumbers.get(i)), String.valueOf(fixed2[i]));
+                        hostportList.add(hostportEntry);
+                    }
+                }
+                else {
+                    for (int i = 0; i < fixedNumbers.size(); i++) {
+                        Map<String, Object> hostportEntry = new HashMap<>();
+                        hostportEntry.put(String.valueOf(fixedNumbers.get(i)), String.valueOf(hostport + i));
+                        hostportList.add(hostportEntry);
                     }
                 }
                 modifiedEntry.put("hostport", hostportList);
